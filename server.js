0fb135550d50e82a364328d408c8333661af23aa
duannx <xuanduannguyen@gmail.com>
2023-06-01 16:06:13 +0700

    add build script

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
 // @ts-check
-import fs from 'node:fs'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import express from 'express'
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import express from "express";
 
-const isTest = process.env.VITEST
+const isTest = process.env.VITEST;
 
 export async function createServer(
   root = process.cwd(),
-  isProd = process.env.NODE_ENV === 'production',
-  hmrPort,
+  isProd = process.env.NODE_ENV === "production",
+  hmrPort
 ) {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url))
-  const resolve = (p) => path.resolve(__dirname, p)
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const resolve = (p) => path.resolve(__dirname, p);
 
   const indexProd = isProd
-    ? fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
-    : ''
+    ? fs.readFileSync(resolve("dist/client/index.html"), "utf-8")
+    : "";
 
   const manifest = isProd
     ? JSON.parse(
-        fs.readFileSync(resolve('dist/client/ssr-manifest.json'), 'utf-8'),
+        fs.readFileSync(resolve("dist/client/ssr-manifest.json"), "utf-8")
       )
-    : {}
+    : {};
 
-  const app = express()
+  const app = express();
 
   /**
    * @type {import('vite').ViteDevServer}
    */
-  let vite
+  let vite;
   if (!isProd) {
     vite = await (
-      await import('vite')
+      await import("vite")
     ).createServer({
-      base: '/test/',
+      base: "/test/",
       root,
-      logLevel: isTest ? 'error' : 'info',
+      logLevel: isTest ? "error" : "info",
       server: {
         middlewareMode: true,
         watch: {
@@ -49,57 +49,60 @@ export async function createServer(
           port: hmrPort,
         },
       },
-      appType: 'custom',
-    })
+      appType: "custom",
+    });
     // use vite's connect instance as middleware
-    app.use(vite.middlewares)
+    app.use(vite.middlewares);
   } else {
-    app.use((await import('compression')).default())
+    app.use((await import("compression")).default());
     app.use(
-      '/test/',
-      (await import('serve-static')).default(resolve('dist/client'), {
+      (await import("serve-static")).default(resolve("dist/client"), {
         index: false,
-      }),
-    )
+      })
+    );
   }
 
-  app.use('*', async (req, res) => {
+  app.use("*", async (req, res) => {
     try {
-      const url = req.originalUrl.replace('/test/', '/')
+      const url = req.originalUrl.replace("/test/", "/");
 
-      let template, render
+      let template, render;
       if (!isProd) {
         // always read fresh template in dev
-        template = fs.readFileSync(resolve('index.html'), 'utf-8')
-        template = await vite.transformIndexHtml(url, template)
-        render = (await vite.ssrLoadModule('/src/entry-server.ts')).render
+        template = fs.readFileSync(resolve("index.html"), "utf-8");
+        template = await vite.transformIndexHtml(url, template);
+        render = (await vite.ssrLoadModule("/src/entry-server.ts")).render;
       } else {
-        template = indexProd
+        template = indexProd;
         // @ts-ignore
-        render = (await import('./dist/server/entry-server.js')).render
+        render = (await import("./dist/server/entry-server.js")).render;
       }
 
-      const [appHtml, preloadLinks] = await render(url, manifest)
+      const [appHtml, preloadLinks, initialState] = await render(url, manifest);
+      // pass initial sate to client to avoid hydration mismatch
+      const initialStateScript = `<script>window.__INITIAL_STATE__=${JSON.stringify(
+        initialState
+      )}</script>`;
 
       const html = template
-        .replace(`<!--preload-links-->`, preloadLinks)
-        .replace(`<!--app-html-->`, appHtml)
+        .replace(`<!--preload-links-->`, `${preloadLinks} ${initialStateScript}`)
+        .replace(`<!--app-html-->`, appHtml);
 
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+      res.status(200).set({ "Content-Type": "text/html" }).end(html);
     } catch (e) {
-      vite && vite.ssrFixStacktrace(e)
-      console.log(e.stack)
-      res.status(500).end(e.stack)
+      vite && vite.ssrFixStacktrace(e);
+      console.log(e.stack);
+      res.status(500).end(e.stack);
     }
-  })
+  });
 
-  return { app, vite }
+  return { app, vite };
 }
 
 if (!isTest) {
   createServer().then(({ app }) =>
     app.listen(6173, () => {
-      console.log('http://localhost:6173')
-    }),
-  )
+      console.log("http://localhost:6173");
+    })
+  );
 }
